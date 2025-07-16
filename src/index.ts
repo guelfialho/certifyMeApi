ef74a5ef92518a44f1f9899cb1975f3a336756f4
Miguel Fialho <miguel@credifit.com.br>
2025-07-16 18:46:42 -0300

    feature: usando a porta 0.0.0.0 para poder rodar o projeto com emulador do android studio

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 API CertifyMe rodando na porta ${PORT}`);
 });
